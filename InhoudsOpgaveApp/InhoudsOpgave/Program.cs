0d2d423090b39ec5ba9f6fb345720ebe35b76492
StanGabrielProE <gabriel.stan@student.pxl.be>
2025-11-11 12:52:37 +0100

    Add: Base Files voor Classes Labo

---
+++ InhoudsOpgaveApp/InhoudsOpgave/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InhoudsOpgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
