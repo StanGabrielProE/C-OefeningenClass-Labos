ddffc74d92840f351f020af9b8d98faf50421c57
StanGabrielProE <gabriel.stan@student.pxl.be>
2025-11-11 15:30:15 +0100

    REFACTOR+Add ConsoleUiHelper

--- InhoudsOpgaveApp/InhoudsOpgave/Models/ConsoleUiHelper.cs
+++ InhoudsOpgaveApp/InhoudsOpgave/Models/ConsoleUiHelper.cs
@@ -6,18 +6,7 @@ namespace InhoudsOpgave.Models;
 
 public static class ConsoleUiHelper<T> where T:INumber<T>
 {
-    public  static DateTime ConvertToDateTime(Func<string> userInput)
-    {
-        
-        DateTime dateOfBirth;
-        string input = userInput();
-        while (!DateTime.TryParseExact(input.Trim(), "dd MM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateOfBirth))
-        {
-            Console.WriteLine("Invalid Date format dd MM yyyy");
-            input = userInput();
-        }
-        return dateOfBirth;
-    }
+
 
 
     public  static T ConvertToNumber(string userInput)
@@ -30,5 +19,35 @@ public static class ConsoleUiHelper<T> where T:INumber<T>
         }
     
         return result;
+    }
+
+
+}
+public static class ConsoleUiHelper
+{
+    public static DateTime ConvertToDateTime(Func<string> userInput)
+    {
+
+        DateTime dateOfBirth;
+        string input = userInput();
+        while (!DateTime.TryParseExact(input.Trim(), "dd MM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateOfBirth))
+        {
+            Console.WriteLine("Invalid Date format dd MM yyyy");
+            input = userInput();
+        }
+        return dateOfBirth;
+    }
+    public static string NameValidator(Func<string> input)
+    {
+        string name = input();
+
+        while (string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine("Wrong format");
+            name = input();
+        }
+        return name;
+
+
     }
 }
