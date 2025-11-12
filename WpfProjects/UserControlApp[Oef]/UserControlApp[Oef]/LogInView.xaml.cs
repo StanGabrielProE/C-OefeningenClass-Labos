5bdc2762313fac034bbbf85be67ed3afd3384b84
StanGabrielProE <gabriel.stan@student.pxl.be>
2025-11-12 17:25:54 +0100

    Oef WPF

---
+++ WpfProjects/UserControlApp[Oef]/UserControlApp[Oef]/LogInView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlApp_Oef_
+{
+    /// <summary>
+    /// Interaction logic for LogInView.xaml
+    /// </summary>
+    public partial class LogInView : UserControl
+    {
+        public LogInView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.Content = new InvoiceView();
+        }
+    }
+}
